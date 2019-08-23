dd32802176103803cef76f9699022636fdec6732
miftah93 <54450040+miftah93@users.noreply.github.com>
2019-08-24 00:38:43 +0700

    Update and rename README.md to cfg.php

--- README.md
+++
@@ -1,2 +0,0 @@
-# cashzin
-belajar lah dengan jujur
---
+++ cfg.php
@@ -0,0 +1,4 @@
+<?php
+
+$data = '{"articleid":"135183","userid":"1051797","Infversion":"1.0","Key":"QZasPErgVnhzlhEFYougu1OZ+QaaOx7iUe+O+vrfdfRBcAdZEQ+/wg==","Method":"ArticleValidReadReward","UID":"1566161837080","appv":"3.40","country":"Indonesia","i18n":"id_ID","sourceOrigin":"GooglePlay","systemtype":"2"}';
+
